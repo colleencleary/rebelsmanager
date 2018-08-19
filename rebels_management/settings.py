9968ef1e78fe0c1aaa7910355c22f977b19b717a
colleencleary <cleary.ce@gmail.com>
2018-08-19 12:00:14 -0400

    heroku testing

--- rebels_management/settings.py
+++ rebels_management/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-# import django_heroku
-import dj_database_url
+# import dj_database_url
 import os
 import django_heroku
 
